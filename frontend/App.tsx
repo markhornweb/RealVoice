4b0e02146dd614390e0e398afed08e382e43d0ba
markhornweb <MarkHorn_Web@outlook.com>
2024-02-16 07:54:58 +0530

    first commit

---
+++ frontend/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import { NativeRouter, Route, Link } from "react-router-native";
+
+function App(): JSX.Element {
+  return (
+    <NativeRouter>
+      <View>
+        
+      </View>
+    </NativeRouter>
+  );
+}
+
+export default App;
